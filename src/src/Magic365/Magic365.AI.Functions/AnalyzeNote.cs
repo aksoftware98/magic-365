87ab8124b83db2acad5e9d4d920ee932c81aa81e
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-06-15 18:52:11 +0400

    Create the Azure to analyze the note using Open AI GPT API

---
+++ src/src/Magic365/Magic365.AI.Functions/AnalyzeNote.cs
@@ -0,0 +1,135 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Magic365.AI.Functions
+{
+    public class AnalyzeNote
+    {
+
+        private readonly IConfiguration _configuration;
+
+		public AnalyzeNote(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		[FunctionName("Function1")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            // API Key  
+
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            if (string.IsNullOrWhiteSpace(body))
+                return new BadRequestObjectResult(new
+                {
+                    message = "Request body is required"
+                });
+
+            var noteRequest = JsonSerializer.Deserialize<NoteRequest>(body);
+            if (string.IsNullOrWhiteSpace(noteRequest.Message))
+            {
+				return new BadRequestObjectResult(new
+				{
+					message = "Message content is required, You can set the value by setting the property 'message' in the body"
+				});
+			}    
+
+            if (noteRequest.Message.Length > 2000)
+            {
+				return new BadRequestObjectResult(new
+				{
+					message = "Message content cannot be more than 2000 characters"
+				});
+			}
+
+            var apiKey = _configuration["ApiKey"];
+			string prompt = @"Analyze the following plan as separate sentences and respond to me a JSON array containing list of objects, each object has the following properties: type, action, startDate, startTime, endDate, endTime, people
+                            ---
+                            user Message here
+                            ---
+
+                            Each sentence must be a type of three: ToDoItem, Event, Meeting. the sentence must be only one type and cannot have two types at the same time. 
+                            the ToDoItem sentence is the sentence that represents actions and doesn't contain a time or people.
+                            the Event is the sentence that represents action but with a time and doesn't contain people. 
+                            the Meeting is like Event but it has people's names involved. 
+                            action property is the actual action the user wants in the sentence.
+                            define the startDate and endDate for the Event and the meeting and print it in the following format ""yyyy-MM-dd"" and if the date is not specific set it to null. endDate if it's not defined also set it to null. 
+                            for the startTime and endTime for the Event and the Meeting should be in the following format ""hh:mm:ss tt"" and if the endTime is not specific calculate at 30 mins from the startTime. 
+                            The Meeting sentence contains should contain at least one person name, so show this name as JSON string array in the object. 
+                            The final output should be like the following example
+                            ---
+                            [
+                              {
+                                ""type"": ""ToDoItem"",
+                                ""action"": ""Go to the super market"",
+                                ""startDate"": ""2023-06-18""
+                              },
+                              {
+                                ""type"": ""Event"",
+                                ""startDate"": ""2023-06-18"",
+                                ""action"": ""Have a coffee"",
+                                ""startTime"": ""04:00 PM"",
+                                ""endDate"": ""2023-06-18"",
+                                ""endTime"": ""04:30 PM""
+                              },
+                              {
+                                ""type"": ""Meeting"",
+                                ""startDate"": ""2023-06-18"",
+                                ""action"": ""Study AI"",
+                                ""startTime"": ""06:00 PM"",
+                                ""endDate"": ""2023-06-18"",
+                                ""endTime"": ""08:00 PM"",
+                                ""people"": [""Ahmad""]
+                              }
+                            ]
+                            ---";
+
+			using var client = new HttpClient(); 
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
+            var response = await client.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[] {
+                    new
+                    {
+                        role = "system", 
+                        content = prompt
+                    },
+                    new
+                    {
+                        role = "user",
+                        content = ""
+					}
+                }
+            });
+            log.LogError(await response.Content.ReadAsStringAsync());
+
+            return new OkObjectResult(await response.Content.ReadFromJsonAsync<object>());
+        }
+    }
+
+    public class NoteRequest
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+    }
+
+    public class NoteResponse
+    {
+
+    }
+}
