c694a056ef19a3518ad3b38795dddfe21055d091
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-07-08 23:35:31 +0400

    Add App Center analytics for the Windows app

---
+++ src/Magic365.WinUI/Secrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic365.WinUI;
+internal static class Secrets
+{
+
+    public const string AppCenterId = "APP_CENTER_SECRET_ID";
+
+}
